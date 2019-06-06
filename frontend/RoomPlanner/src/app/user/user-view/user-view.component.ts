93fe43d6fa796004d8663802c31dffdd9f8dc301
iulianaluncanu <luncanuiuliana@gmail.com>
2019-06-06 14:15:28 +0300

    created layout for user-view

--- frontend/RoomPlanner/src/app/user/user-view/user-view.component.ts
+++ frontend/RoomPlanner/src/app/user/user-view/user-view.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserViewComponent implements OnInit {
 
+  dropdownOpen: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleDropdown(){
+    this.dropdownOpen = !this.dropdownOpen;
+  }
 }
