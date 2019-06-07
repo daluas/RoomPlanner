44a931e53b9353daf01ee1b4a913082c2190cd9c
iulianaluncanu <luncanuiuliana@gmail.com>
2019-06-07 14:40:35 +0300

    created calendar

--- frontend/RoomPlanner/src/app/material-design/material-design.module.ts
+++ frontend/RoomPlanner/src/app/material-design/material-design.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { MatDatepickerModule, MatNativeDateModule  } from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule 
+
   ],
   exports:[
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatDatepickerModule
   ]
 })
 export class MaterialDesignModule { }
--- frontend/RoomPlanner/src/app/user/user-view/user-view.component.ts
+++ frontend/RoomPlanner/src/app/user/user-view/user-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatCalendar,DateAdapter } from '@angular/material';
 
 @Component({
   selector: 'app-user-view',
@@ -8,13 +9,44 @@ import { Component, OnInit } from '@angular/core';
 export class UserViewComponent implements OnInit {
 
   dropdownOpen: boolean = false;
+  dateChanged:boolean=false;
 
-  constructor() { }
+  constructor(private _dateAdapter: DateAdapter<Date>) { }
 
   ngOnInit() {
+    
   }
 
   toggleDropdown(){
     this.dropdownOpen = !this.dropdownOpen;
   }
+
+  defaultDate: Date = this._dateAdapter.today();;
+  selectedDate: Date;
+
+  onDateChanged(date) {
+    this.dateChanged=true;
+  }
+
+  onApplyFilters(){
+    if(this.dateChanged){
+      console.log(`The date is: ${this.selectedDate}`);
+    }
+    else{
+      console.log(`The date is: ${this.defaultDate}`);
+    }
+    // var datee=[];
+    // var datee2=[];
+    // for(let i=0;i<30;i++){
+    //   datee[i]=document.getElementsByClassName("mat-calendar-body-cell-content")[i];
+    //   datee2[i]=document.getElementsByClassName("mat-calendar-body-cell-content")[i].firstChild;
+      
+    //   if((parseInt(datee2[i], 10))<7){
+    //     datee[i].classList.add('example-custom-date-class');
+    //   }
+    //   console.log(datee[i]);
+    
+  }
+
+ 
 }
