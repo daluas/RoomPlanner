96a8e5a6a67b8ec13c57735131d800c8b1b666af
iulianaluncanu <luncanuiuliana@gmail.com>
2019-06-11 12:14:41 +0300

    completed tests for calendar

--- frontend/RoomPlanner/src/app/material-design/material-design.module.ts
+++ frontend/RoomPlanner/src/app/material-design/material-design.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule, MatNativeDateModule } from '@angular/material';
-import { MatFormFieldModule, MatInputModule, MatChipInput } from '@angular/material';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -14,24 +15,25 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     CommonModule,
     MatToolbarModule,
     MatButtonModule,
+    MatSelectModule,
     MatDatepickerModule,
     MatNativeDateModule,
     MatFormFieldModule,
     MatInputModule,
     MatCardModule,
     MatIconModule,
-    MatButtonModule,
     MatProgressSpinnerModule
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
     MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
     MatFormFieldModule,
     MatInputModule,
     MatCardModule,
     MatIconModule,
-    MatButtonModule,
     MatProgressSpinnerModule
   ]
 })
--- frontend/RoomPlanner/src/app/user/user-view/user-view.component.spec.ts
+++ frontend/RoomPlanner/src/app/user/user-view/user-view.component.spec.ts
@@ -1,16 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserViewComponent } from './user-view.component';
+import { MaterialDesignModule } from '../../material-design/material-design.module';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('UserViewComponent', () => {
+fdescribe('UserViewComponent', () => {
   let component: UserViewComponent;
   let fixture: ComponentFixture<UserViewComponent>;
 
+  function getButton(): DebugElement {
+    return fixture.debugElement.query(By.css('button'));
+  }
+
+  function getCalendar(): DebugElement {
+    return fixture.debugElement.query(By.css('mat-calendar'));
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserViewComponent ]
+      declarations: [
+        UserViewComponent
+      ],
+      imports: [
+        MaterialDesignModule,
+        BrowserAnimationsModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +40,57 @@ describe('UserViewComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('default day should be the current day', () => {
+    var date=new Date();
+    expect(component.defaultDate.toString).toEqual(date.toString);
+  });
+
+  it('should call **onApplyFilters** when button is clicked ', () => {
+
+    const elem: DebugElement = getButton();
+    spyOn(component, 'onApplyFilters');
+
+    elem.nativeElement.click();
+
+    fixture.whenStable().then(() => {
+      expect(component.onApplyFilters).toHaveBeenCalled();
+    });
+    
+  });
+
+  it('should call **onDateChanged** with date picked by user when calendar is clicked ', () => {
+
+    var newDate=new Date(2019,6,16);
+
+    const calendarElem:DebugElement=getCalendar();
+   
+    spyOn(component,'onDateChanged');
+
+    calendarElem.nativeElement.selected=newDate;
+
+    fixture.whenStable().then(() => {
+      expect(component.onDateChanged).toHaveBeenCalledWith(newDate);
+    });
+  });
+
+  
+  it('should return the date picked by user when button "Apply Filters" is clicked ', () => {
+
+    var newDate=new Date(2019,6,16);
+
+    const calendarElem:DebugElement=getCalendar();
+    calendarElem.nativeElement.selected=newDate;
+
+    const elem: DebugElement = getButton();
+    elem.nativeElement.click();
+
+    spyOn(component, 'getReturnDate');
+
+    fixture.whenStable().then(() => {
+      expect(component.getReturnDate().valueOf).toEqual(newDate);
+    });
+  });
+
+
 });
