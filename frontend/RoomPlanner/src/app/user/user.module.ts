ee983fbdcf3ab6b57cb6061b1946ddf1e1243c4a
bootcamp-PC8\bootcamp <emanuel.borcila@cegeka.com>
2019-06-14 12:26:53 +0300

    moved filters code from user view component to new filters component

--- frontend/RoomPlanner/src/app/user/user.module.ts
+++ frontend/RoomPlanner/src/app/user/user.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { UserViewComponent } from './user-view/user-view.component';
 import { MaterialDesignModule } from '../material-design/material-design.module';
+import { FiltersComponent } from './user-view/filters/filters.component';
 
 @NgModule({
-  declarations: [UserViewComponent],
+  declarations: [UserViewComponent, FiltersComponent],
   imports: [
     CommonModule,
     UserRoutingModule,
