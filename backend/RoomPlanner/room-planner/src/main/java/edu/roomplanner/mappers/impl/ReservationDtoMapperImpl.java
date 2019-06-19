a9b28950480b42cf5c5030ee5173ef7db3bd5f1b
Irina Onea <ilinca.asandului@cegeka.com>
2019-06-19 14:57:49 +0300

    test

---
+++ backend/RoomPlanner/room-planner/src/main/java/edu/roomplanner/mappers/impl/ReservationDtoMapperImpl.java
@@ -0,0 +1,26 @@
+package edu.roomplanner.mappers.impl;
+
+import edu.roomplanner.dto.ReservationDto;
+import edu.roomplanner.entity.ReservationEntity;
+import edu.roomplanner.mappers.ReservationDtoMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationDtoMapperImpl implements ReservationDtoMapper {
+
+    public ReservationDto mapReservationEntityToDto(ReservationEntity reservationEntity) {
+        ReservationDto reservationDto = new ReservationDto();
+        reservationDto.setStartDate(reservationEntity.getStartDate());
+        reservationDto.setEndDate(reservationEntity.getEndDate());
+        reservationDto.setDescription(reservationEntity.getDescription());
+        return reservationDto;
+    }
+
+    public ReservationEntity mapReservationDtoToEntity(ReservationDto reservationDto) {
+        ReservationEntity reservationEntity = new ReservationEntity();
+        reservationEntity.setStartDate(reservationDto.getStartDate());
+        reservationEntity.setEndDate(reservationDto.getEndDate());
+        reservationEntity.setDescription(reservationDto.getDescription());
+        return reservationEntity;
+    }
+}
