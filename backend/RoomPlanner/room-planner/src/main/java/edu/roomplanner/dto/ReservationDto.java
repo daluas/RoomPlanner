18199645cc34c913f070417b121855c97a08d73e
Irina Onea <ilinca.asandului@cegeka.com>
2019-06-19 14:57:28 +0300

    test

--- backend/RoomPlanner/room-planner/src/main/java/edu/roomplanner/dto/ReservationDto.java
+++ backend/RoomPlanner/room-planner/src/main/java/edu/roomplanner/dto/ReservationDto.java
@@ -1,17 +1,24 @@
 package edu.roomplanner.dto;
 
-import edu.roomplanner.entity.UserEntity;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.stereotype.Component;
 
 import java.util.Calendar;
 
+@JsonSerialize
 @Getter
 @Setter
+@EqualsAndHashCode
+@Component
 public class ReservationDto {
-    private Long id;
+
+    private Long personId;
     private Calendar startDate;
     private Calendar endDate;
     private String description;
-    private UserEntity person;
+
+
 }
