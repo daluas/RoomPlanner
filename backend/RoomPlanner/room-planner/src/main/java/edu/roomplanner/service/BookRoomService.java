d9f084de6a3699295e421238d4bec492640c831a
Irina Onea <ilinca.asandului@cegeka.com>
2019-06-19 14:58:53 +0300

    test

---
+++ backend/RoomPlanner/room-planner/src/main/java/edu/roomplanner/service/BookRoomService.java
@@ -0,0 +1,13 @@
+package edu.roomplanner.service;
+
+import edu.roomplanner.dto.ReservationDto;
+import edu.roomplanner.entity.ReservationEntity;
+
+public interface BookRoomService {
+    ReservationEntity createReservation(ReservationEntity reservationEntity);
+
+    ReservationEntity convertToEntity(ReservationDto reservationDto);
+
+    ReservationDto convertToDto(ReservationEntity reservationEntity);
+
+}
